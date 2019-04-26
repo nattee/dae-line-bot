3b1ca8383272c3d3675c0a6081d6c09f29e2619f
Nattee Niparnan <nattee@gmail.com>
2019-04-26 17:40:58 +0700

    start the api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,49 @@
 class ApiController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  before_action :setup_client
+
+  require 'line/bot'
+
   def test
+  end
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless @client.validate_signature(body, signature)
+      render plain: "Bad Request", content_type: 'text/plain', status: 400
+      return
+    end
+
+    events = @client.parse_events_from(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          @client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = @client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    }
+
+    render plain: "OK", content_type: 'text/plain'
+  end
+
+  private
+  def setup_client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = Rails.application.secrets.LINE_CHANNEL_SECRET
+      config.channel_token = Rails.application.secrets.LINE_CHANNEL_TOKEN
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'api/test'
+  post 'api/callback'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'test', to: 'api#test'
 end
