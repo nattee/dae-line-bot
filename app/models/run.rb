4cbade141b0b45717155ab456bf46ecaf087a0c3
Nattee Niparnan <nattee@gmail.com>
2019-08-02 01:12:35 +0700

    cascading delete

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,5 +1,6 @@
 class Run < ApplicationRecord
   belongs_to :athlete
   belongs_to :course
-  has_many :plans
+  has_many :plans, dependent: :destroy_all
+  has_many :checkins, dependent: :destroy_all
 end
