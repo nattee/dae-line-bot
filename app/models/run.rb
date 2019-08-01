9dc98dec73a56b8b7267f3e4f71487075cb86864
Nattee Niparnan <nattee@gmail.com>
2019-08-02 01:15:41 +0700

    cascade

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,6 +1,6 @@
 class Run < ApplicationRecord
   belongs_to :athlete
   belongs_to :course
-  has_many :plans, dependent: :destroy_all
-  has_many :checkins, dependent: :destroy_all
+  has_many :plans, dependent: :destroy
+  has_many :checkins, dependent: :destroy
 end
